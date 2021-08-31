b1b57c1f82b2dc01515b82d4bb016a15af03a1de
DESKTOP-IJ3V1EB\vantan <tkm.y0919@icloud.com>
2021-08-31 16:17:23 +0900

    提出物追加

---
+++ Assets/TimeCounter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimeCounter : MonoBehaviour
+{
+    //カウントダウン
+    public float time = 10.0f;
+    public Text timerText;
+    public bool isTimeUp;
+    //時間を表示するText型の変数
+    public Text timeText;
+    // Update is called once per frame
+    private void Start()
+    {
+        isTimeUp = false;
+    }
+    void Update()
+    {
+        if (0 < time)
+        {
+            //時間をカウントダウンする
+            time -= Time.deltaTime;
+            //時間を表示する
+            timeText.text = time.ToString("f1") + "秒";
+        }
+        //countdownが0以下になったとき
+        else if (time <= 0)
+        {
+            isTimeUp = true;
+            //timeText.text = "Game Over";
+            SceneManager.LoadScene("Scenes/a]");
+        }
+    }
+}
